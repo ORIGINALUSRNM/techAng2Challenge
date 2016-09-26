5d086f3f1afb2861513e79682d71fbb4b4ed55b0
Jason Hornbuckle <jshornbuckle@gmail.com>
2016-09-25 22:42:59 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ComboboxComponent } from './combobox/combobox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComboboxComponent
   ],
   imports: [
     BrowserModule,
